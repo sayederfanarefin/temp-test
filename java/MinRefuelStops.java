03b0c452d9760a4089d1f2c0cee375d83efe1113
Sayed Erfan Arefin <erfanjordison>
2025-10-03 05:13:07 -0400

    java test

--- java/MinRefuelStops.java
+++ java/MinRefuelStops.java
@@ -4,19 +4,20 @@ import java.util.*;
 
 public class MinRefuelStops {
     public static int minRefuelStops(int target, int startFuel, int[][] stations) {
-        long fuel = startFuel;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        int i = 0, stops = 0;
-        while (fuel < target) {
-            while (i < stations.length && stations[i][0] <= fuel) {
-                pq.offer(stations[i][1]);
-                i=i+1;
-            }
-            if (pq.isEmpty()) return -1;
-            fuel += pq.poll();
-            stops++;
+    long fuel = startFuel;
+    PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+    int i = 0, stops = 0;
+    while (fuel < target) {
+        while (i < stations.length && stations[i][0] <= fuel) {
+            pq.offer(stations[i][1]);
+            i++;
         }
-        return stops;
+        if (pq.isEmpty()) return -1;
+        fuel += pq.poll();
+        stops++;
     }
+    return stops;
+}
+
 }
 
