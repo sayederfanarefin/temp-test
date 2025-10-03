6e6bc84da50ae193bb48d9ec88b6c99a7aa551da
Sayed Erfan Arefin <erfanjordison>
2025-10-03 04:52:37 -0400

    python test

--- java/MinRefuelStops.java
+++ java/MinRefuelStops.java
@@ -10,7 +10,7 @@ public class MinRefuelStops {
         while (fuel < target) {
             while (i < stations.length && stations[i][0] <= fuel) {
                 pq.offer(stations[i][1]);
-                i++;
+                i=i+2;
             }
             if (pq.isEmpty()) return -1;
             fuel += pq.poll();
