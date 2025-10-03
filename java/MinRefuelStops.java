2d39d199774dc41dea38a40c180def60ff9151a6
Sayed Erfan Arefin <erfanjordison>
2025-10-03 04:50:40 -0400

    python test

--- java/MinRefuelStops.java
+++ java/MinRefuelStops.java
@@ -2,7 +2,25 @@ import java.util.*;
 
 public class MinRefuelStops {
     public static int minRefuelStops(int target, int startFuel, int[][] stations) {
-        // TODO: Implement greedy with max-heap (PriorityQueue in reverse order)
-        throw new UnsupportedOperationException("Not implemented yet");
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        int fuel = startFuel;
+        int stops = 0;
+        int index = 0;
+
+        while (fuel < target) {
+            while (index < stations.length && stations[index][0] <= fuel) {
+                maxHeap.offer(stations[index][1]);
+                index++;
+            }
+
+            if (maxHeap.isEmpty()) {
+                return -1;
+            }
+
+            fuel += maxHeap.poll();
+            stops++;
+        }
+
+        return stops;
     }
 }
